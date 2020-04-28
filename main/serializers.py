58e1001dd8a9587b01420387a6e0670b3b067536
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-04-28 13:33:18 -0700

    players, room, items, and season endpoints working.

--- main/serializers.py
+++ main/serializers.py
@@ -5,3 +5,18 @@ class PlayersSerializer(serializers.ModelSerializer):
   class Meta:
     model = Players
     fields = '__all__'
+
+class RoomSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Room
+    fields = '__all__'
+
+class ItemsSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Items
+    fields = '__all__'
+
+class SeasonSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Season
+    fields = '__all__'
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from rest_framework import routers
-from .api import PlayersViewSet
+from .api import PlayersViewSet, RoomViewSet, ItemsViewSet, SeasonViewSet
 
 router = routers.DefaultRouter()
 router.register('api/players', PlayersViewSet, 'players')
+router.register('api/room', RoomViewSet, 'room')
+router.register('api/items', ItemsViewSet, 'items')
+router.register('api/season', SeasonViewSet, 'season')
+
 
 urlpatterns = router.urls
